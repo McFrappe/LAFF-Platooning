9aba4b1cbb88ad8e7a2bd87809906d83ebcd688a
Fredrik Engstrand <fredrik@engstrand.nu>
2023-11-08 13:57:30 +0100

    fix: use max and left angle constants

--- src/rcv/nodes/joystick_controller.py
+++ src/rcv/nodes/joystick_controller.py
@@ -38,15 +38,15 @@ class JoystickController:
         #rospy.loginfo(f"Data from controller -> {data}")
         # https://github.com/naoki-mizuno/ds4_driver/blob/humble-devel/ds4_driver_msgs/msg/Status.msg
         # Left: 1.0, Right: -1.0, Up: 1.0, Down: -1.0
-        steer_axis_x = data.axes[0] 
-        speed_axis_y = data.axes[3] 
+        steer_axis_x = data.axes[0]
+        speed_axis_y = data.axes[3]
         reverse_speed = data.axes[4]
         forward_speed = data.axes[5]
-        
+
         # Buttons (0: Not pressed, 1: Pressed)
         button_dpad_left = data.buttons[-4]
         button_dpad_right = data.buttons[-2]
-        
+
         # Change zero angle after trim input deom dpad
         if button_dpad_left == 1:
             self.zero = max(self.zero - 1, self.max_left)
@@ -58,10 +58,7 @@ class JoystickController:
         else:
             self.current_speed = int(np.interp(reverse_speed, [0, 1], [self.idle, self.max_reverse]))
 
-        #self.current_speed = int(np.interp(speed_axis_y, [-1, 1], [self.idle, self.max_forward]))
-        # TODO: Might need to check and map left and right separately 
-        #self.steering_angle = int(np.interp(steer_axis_x, [-1, 1], [self.max_right, self.max_left]))
-        self.steering_angle = int(np.interp(steer_axis_x, [-1, 1], [self.zero + 30, self.zero - 30]))
+        self.steering_angle = int(np.interp(steer_axis_x, [-1, 1], [self.max_right, self.max_left]))
 
     def perform_step(self, event):
         self.speed_publisher.publish(self.current_speed)
