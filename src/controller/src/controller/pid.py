8fe955ef0d0470ce54ef0158cad40d19be443cb2
Anton Augustsson <anton.augustsson99@gmail.com>
2023-11-30 13:34:47 +0100

    initial pwm and velocity pid controller

--- src/controller/src/controller/pid.py
+++ src/controller/src/controller/pid.py
@@ -1,8 +1,22 @@
-from simple_pid import PID as SimplePID
-
 class PID:
-    def __init__(self, kp, ki, kd, setpoint: int):
-        self.pid = SimplePID(Kp=kp, Ki=ki, Kd=kd, setpoint=setpoint)
+    # TODO: pid_min and pid_max should be optional
+    def __init__(self, kp, ki, kd, dt: int, pid_min: float, pid_max: float):
+        self.error = 0
+        self.error_integral = 0
+        self.error_derivative = 0 
+        self.Kp = kp
+        self.Ki = ki
+        self.Kd = kd
+        self.dt = dt
+        self.pid_min = pid_min
+        self.pid_max = pid_max
 
-    def update(self, distance: float):
-        return self.pid(distance)
+    def update(self, error: float):
+        error_prev = self.error
+        self.error = error
+        self.error_integral += error*self.dt
+        self.error_derivative = (error - error_prev)/self.dt
+
+        u = self.Kp*error + self.Ki*self.error_integral + self.Kd*self.error_derivative
+        return min(max(u, self.pid_min), self.pid_max)
+
--- src/rcv/nodes/pid_controller.py
+++ src/rcv/nodes/pid_controller.py
@@ -9,15 +9,26 @@ from controller.pid import PID
 
 class PIDController:
     def __init__(self):
+        self.__period = rospy.get_param("PID_CONTROL_PERIOD")
         self.__id = rospy.get_param("VEHICLE_ID")
-        self.__pid = PID(
-            rospy.get_param("K_P"),
-            rospy.get_param("K_I"),
-            rospy.get_param("K_D"),
-            rospy.get_param("PID_REFERENCE"))
+        self.__pid_platooning = PID(
+            rospy.get_param("K_PP"),
+            rospy.get_param("K_PI"),
+            rospy.get_param("K_PD"),
+            self.__period, # FIXME: should this be DISTANCE_PUBLISH_PERIOD?
+            rospy.get_param("PID_PMIN"),
+            rospy.get_param("PID_PMAX"))
 
-        self.__pid_min = rospy.get_param("PID_MIN")
-        self.__pid_max = rospy.get_param("PID_MAX")
+        self.__pid_speed = PID(
+            rospy.get_param("K_SP"),
+            rospy.get_param("K_SI"),
+            rospy.get_param("K_SD"),
+            self.__period, # FIXME: should this be VELOCITY_PUBLISH_PERIOD?
+            rospy.get_param("PID_SMIN"),
+            rospy.get_param("PID_SMAX"))
+
+        self.__velocity_min = rospy.get_param("VELOCITY_MIN")
+        self.__velocity_max = rospy.get_param("VELOCITY_MAX")
 
         self.__max_forward = rospy.get_param("MAX_FORWARD_MOTOR")
         self.__max_reverse = rospy.get_param("MAX_REVERSE_MOTOR")
@@ -27,17 +38,19 @@ class PIDController:
         self.__zero = rospy.get_param("ZERO_ANGLE")
 
         self.__steering_angle = self.__zero
-        self.__current_speed = self.__idle
+        self.__current_velocity = 0
+        self.__reference_velocity = 0
+        self.__current_pwm = self.__idle
         self.__current_distance = 0
 
         self.__message_queue_size = rospy.get_param("MESSAGE_QUEUE_SIZE")
 
-        self.speed_publisher = rospy.Publisher(
+        self.speed_publisher = rospy.Publisher( # TODO: change to pwm_speed
             f"{self.__id}/speed",
             Int32,
             queue_size=self.__message_queue_size)
 
-        self.pid_publisher = rospy.Publisher(
+        self.pid_publisher = rospy.Publisher( # TODO: change to velocity or something
             f"{self.__id}/pid",
             Float32,
             queue_size=self.__message_queue_size)
@@ -53,7 +66,14 @@ class PIDController:
             self.__callback_distance,
             queue_size=self.__message_queue_size)
 
-        rospy.Timer(rospy.Duration(rospy.get_param("PID_CONTROL_PERIOD")), self.__perform_step)
+        self.velocity_subscriber = rospy.Subscriber(
+            f"{self.__id}/velocity",
+            Float32,
+            self.__callback_velocity,
+            queue_size=self.__message_queue_size)
+
+
+        rospy.Timer(rospy.Duration(self.__period), self.__perform_step)
 
     def __callback_distance(self, data: Range):
         """
@@ -61,15 +81,33 @@ class PIDController:
         """
         self.__current_distance = data.range
 
+    def __callback_velocity(self, data: Float32):
+        """
+        Callback for the velocity subscriber.
+        """
+        self.__current_velocity = data.Float32 # FIXME: is this right?
+
+    def __min_distance(self):
+        speed_in_m_per_s = self.__current_velocity/3.6
+        margin_in_m = 0.2 # TODO: should be in vehicle.launch # TODO: should depend on speed and vehicle
+        self.min_distance = speed_in_m_per_s * self.__period *2 + margin_in_m 
+        return self.min_distance
+
     def __perform_step(self, event):
-        updated_control = self.__pid.update(self.__current_distance)
-        self.current_speed = int(np.interp(
-            updated_control,
-            [self.__pid_min, self.__pid_max],
-            [self.__idle, self.__max_forward]
-        ))
-        self.speed_publisher.publish(self.current_speed)
-        self.pid_publisher.publish(updated_control)
+        distance_error = self.__current_distance - self.__min_distance()
+        platoon_control_output = self.__pid_platooning.update(distance_error)
+        desired_velocity = self.__current_velocity+platoon_control_output
+        self.__reference_velocity = min(max(desired_velocity, self.__velocity_min), 
+                    self.__velocity_max)
+
+        speed_error = self.__reference_velocity - self.__current_velocity
+        pwm_control_output = self.__pid_speed.update(speed_error)
+        desired_pwm = self.__current_pwm+pwm_control_output
+        self.__current_pwm = min(max(desired_pwm, self.__idle),
+                  self.__max_forward)
+
+        self.speed_publisher.publish(self.__current_pwm) # FIXME: is this correct
+        self.pid_publisher.publish(self.__reference_velocity) # FIXME: is this correct?
 
     def stop(self):
         self.speed_publisher.publish(self.__idle)
