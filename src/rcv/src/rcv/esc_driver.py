e716c6a3b06a032268c627f7dcc3326ba06f83c9
Fredrik Engstrand <fredrik@engstrand.nu>
2023-12-04 15:25:10 +0100

    fix: use hardware PWM and adjust PWM levels

--- src/rcv/src/rcv/esc_driver.py
+++ src/rcv/src/rcv/esc_driver.py
@@ -11,6 +11,7 @@ class ESCDriver:
         """
         self.__pin = out_pin
         self.__pi = pigpio.pi()
+        self.__freq = rospy.get_param("PWM_FREQUENCY_MOTOR")
         self.__setup_pins()
 
     def __setup_pins(self) -> None:
@@ -18,14 +19,12 @@ class ESCDriver:
         Setup the pins for the  array driver.
         """
         self.__pi.set_mode(self.__pin, pigpio.OUTPUT)
-        self.__pi.set_PWM_frequency(self.__pin, rospy.get_param("PWM_FREQUENCY_MOTOR"))
-        self.__pi.set_PWM_dutycycle(self.__pin, 0)
 
     def set_pwm(self, pwm: int) -> None:
         """
         Give a speed that the motor will try to reach.
         """
-        self.__pi.set_PWM_dutycycle(self.__pin, pwm)
+        self.__pi.hardware_PWM(self.__pin, self.__freq, pwm)
 
     def cleanup(self):
         self.__pi.stop()
