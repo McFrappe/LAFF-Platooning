d1990a67b17d63f3459bf96b513b3231412e07e7
Fredrik Engstrand <fredrik@engstrand.nu>
2023-12-19 13:37:48 +0100

    feat: map velocity to pwm based on minimum velocity in map

--- src/rcv/src/rcv/velocity_mapper.py
+++ src/rcv/src/rcv/velocity_mapper.py
@@ -31,8 +31,18 @@ class VelocityMapper:
         idle and maximum forward, and any velocities smaller or
         larger than the mapped velocities will return these values.
         """
-        if velocity < self.__min_velocity:
+        if velocity <= 0:
             return self.__idle
+
+        # The velocity PWM map does not exactly correspond to the actual
+        # velocity, since it is being tested without the wheels touching
+        # any surface. Therefore, the wheels can spin faster.
+        #
+        # This ensures that if we want to achieve a velocity higher than 0,
+        # the vehicle will start moving. The controller will then adjust the
+        # PWM based on the velocity feedback.
+        if velocity <= self.__min_velocity:
+            velocity = self.__min_velocity
 
         if velocity > self.__max_velocity:
             return self.__max_forward
